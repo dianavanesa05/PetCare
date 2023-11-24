8ced03f7c1320b0be1aa2eee2657bf071b71a188
Vinh Nguyen <vinh28062002@gmail.com>
2023-11-24 16:07:45 -0500

    add community

---
+++ app/src/main/java/com/csc211/petcare/community/Community2Fragment.kt
@@ -0,0 +1,29 @@
+package com.csc211.petcare.community
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.csc211.petcare.R
+import com.csc211.petcare.databinding.FragmentCommunity2Binding
+
+class Community2Fragment : Fragment() {
+    private var _binding: FragmentCommunity2Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCommunity2Binding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/csc211/petcare/community/Community3Fragment.kt
@@ -0,0 +1,29 @@
+package com.csc211.petcare.community
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.csc211.petcare.R
+import com.csc211.petcare.databinding.FragmentCommunity3Binding
+
+class Community3Fragment : Fragment() {
+    private var _binding: FragmentCommunity3Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCommunity3Binding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
