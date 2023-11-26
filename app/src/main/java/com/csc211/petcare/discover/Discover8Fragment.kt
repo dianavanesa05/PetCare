e9c0f03d301bdb9bd39be5ede5770ab53c2469ab
Vinh Nguyen <vinh28062002@gmail.com>
2023-11-26 15:29:07 -0500

    discover

---
+++ app/src/main/java/com/csc211/petcare/discover/Discover8Fragment.kt
@@ -0,0 +1,29 @@
+package com.csc211.petcare.discover
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.csc211.petcare.R
+import com.csc211.petcare.databinding.FragmentDiscover8Binding
+
+class Discover8Fragment: Fragment() {
+    private var _binding: FragmentDiscover8Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDiscover8Binding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
