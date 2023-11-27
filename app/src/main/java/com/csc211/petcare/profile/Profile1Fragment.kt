eaab76a4a7440d7697378c3f3236bf4aa5611bbd
Vinh Nguyen <vinh28062002@gmail.com>
2023-11-27 01:37:31 -0500

    profile 1

---
+++ app/src/main/java/com/csc211/petcare/profile/Profile1Fragment.kt
@@ -0,0 +1,29 @@
+package com.csc211.petcare.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.csc211.petcare.R
+import com.csc211.petcare.databinding.FragmentProfile1Binding
+
+class Profile1Fragment : Fragment() {
+    private var _binding: FragmentProfile1Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfile1Binding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
