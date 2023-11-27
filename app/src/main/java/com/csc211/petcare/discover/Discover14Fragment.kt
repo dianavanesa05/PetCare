c69efd4f92fe880920d9e1e5449c41a0fb85b935
Vinh Nguyen <vinh28062002@gmail.com>
2023-11-26 19:12:18 -0500

    discover screen 14

---
+++ app/src/main/java/com/csc211/petcare/discover/Discover14Fragment.kt
@@ -0,0 +1,29 @@
+package com.csc211.petcare.discover
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.csc211.petcare.R
+import com.csc211.petcare.databinding.FragmentDiscover14Binding
+
+class Discover14Fragment: Fragment() {
+    private var _binding: FragmentDiscover14Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDiscover14Binding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
